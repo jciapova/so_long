b48a947c46b9d1e23ae17a9bfa767f09a82cd680
janka <jana.ciapova@gmail.com>
2023-07-21 18:10:03 +0200

    so long

--- src/so_long.c
+++ src/so_long.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jciapova <jciapova@student.42.fr>          +#+  +:+       +#+        */
+/*   By: janka <janka@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 17:50:34 by jciapova          #+#    #+#             */
-/*   Updated: 2023/07/13 20:06:01 by jciapova         ###   ########.fr       */
+/*   Updated: 2023/07/21 18:07:10 by janka            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include "../get_next_line/get_next_line.h"
 #include <stdio.h>
 #include "../libft/libft.h"
+#include <stdbool.h>
 
 // TO DO = flood fill
 
@@ -32,6 +33,32 @@ int	close_window(void)
 	return (0);
 }
 
+bool flood_fill(t_program *start, int x, int y) 
+{
+	static int	found_exit = 0;
+	static int	collected = 0;
+	if (start->map_data[y][x] == '1')
+		return false;
+	if (start->map_data[y][x] == 'E')
+		found_exit = 1;
+	if (start->map_data[y][x] == 'C') 
+		collected++;
+	start->map_data[y][x] = '1';
+	flood_fill(start, x + 1, y);
+	flood_fill(start, x - 1, y);
+	flood_fill(start, x, y + 1);
+	flood_fill(start, x, y - 1);
+	return (found_exit && collected == start->collectibles_count);
+}
+
+bool check_valid_path(t_program *start) {
+	player_position(start);
+    int player_x = start->player_position_x;
+    int player_y = start->player_position_y;
+
+    return flood_fill(start, player_x, player_y);
+}
+
 int	main(int argc, char **argv)
 {
 	t_program	start;
@@ -44,10 +71,15 @@ int	main(int argc, char **argv)
 		start.mlx = mlx_init();
 		start_map(argv[1], &start);
 		map_size(&start);
+		check_map(&start);
+		if (!check_valid_path(&start)) {
+	  	  	ft_printf("Error: No valid path from the player to the exit\n");
+	   	 	exit(0);
+		}
+		start_map(argv[1], &start);
 		start.win = mlx_new_window(start.mlx, start.map_size_x * 60, \
 		start.map_size_y * 60, "So Long");
 		draw_map(&start);
-		check_map(&start);
 		mlx_hook(start.win, 2, 1L << 0, move_player, &start);
 		mlx_hook(start.win, 17, 1L << 2, close_window, &start);
 		mlx_loop(start.mlx);
